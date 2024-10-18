e61bd9a668c81ce5b7f78e8fb3713615890d1c46
adnene <bendiouisadnane3@gmail.com>
2024-10-18 23:47:00 +0100

    Refactor code to update page count and reset checkboxes and starting to add multiple quiz

--- script.js
+++ script.js
@@ -27,10 +27,9 @@ function handleSubmit() {
 
 function handleNext() {
   switch (counter) {
-
     case 2:
       updatePic.src = "images/image.png";
-      checkboxRight=checkbox3;
+      checkboxRight = checkbox3;
       sol1.textContent = `*p=10, vg=20
                           Dans f_l : vl=30
                           Apres f_l : vg=40
@@ -53,46 +52,54 @@ function handleNext() {
                           vl = 170`;
       break;
     case 3:
-        updatePic.src = "images/3.png";
-        checkboxRight=checkbox3;
-        sol1.textContent = `*p=10, vg=20
-                            Dans f_l : vl=30
-                            Apres f_l : vg=40
-                            Apres f_g: vg=50
-                            vl = 110`;
-        sol2.textContent = `*p=10, vg=20
-                            Dans f_l : vl=30
-                            Apres f_l : vg=40
-                            Apres f_g: vg=50
-                            vl = 140`;
-        sol3.textContent = `*p=10, vg=20
-                            Dans f_l : vl=30
-                            Apres f_l : vg=40
-                            Apres f_g: vg=50
-                            vl = 80`;
-        sol4.textContent = `*p=10, vg=20
-                            Dans f_l : vl=30
-                            Apres f_l : vg=40
-                            Apres f_g: vg=50
-                            vl = 170`;
-        break;
+      updatePic.src = "images/3.png";
+      checkboxRight = checkbox2;
+      sol1.textContent = `1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 12 | 11 | 12 |`;
+      sol2.textContent = `1 | 2 | 3 | 6 | 5 | 4 | 7 | 8 | 9 | 12 | 11 | 12 |`;
+      sol3.textContent = `1 | 2 | 3 | 6 | 5 | 4 | 9 | 8 | 7 | 12 | 11 | 12 |`;
+      sol4.textContent = `1 | 3 | 2 | 6 | 5 | 4 | 9 | 8 | 7 | 12 | 11 | 12 |`;
+      break;
+    case 4:
+      updatePic.src = "images/4.png";
+      checkboxRight = checkbox4;
+      sol1.textContent = `m=o,T[m]=3`;
+      sol2.textContent = `m=L,T[m]=2`;
+      sol3.textContent = `m=l,T[m]=2`;
+      sol4.textContent = `m=T,T[m]=2`;
+      break;
+    case 5:
+      updatePic.src = "images/5.png";
+      checkboxRight = checkbox1;
+      sol1.textContent = `A = -13
+                            A = -16
+                            A = -19
+                            Faux`;
+      sol2.textContent = `A = -12
+                          A = -15
+                          A = -18
+                          Vrai`;
+      sol3.textContent = `A = -12
+                          A = -15
+                          A = -18
+                          Faux`;   
+      sol4.textContent = ``;                  
       break;
     default:
       break;
   }
-  [checkbox1, checkbox2, checkbox3, checkbox4].forEach(checkbox => {
+  [checkbox1, checkbox2, checkbox3, checkbox4].forEach((checkbox) => {
     checkbox.checked = false;
   });
   pagecount.textContent = `${counter}/10`;
 }
 function uncheckOthers(selectedCheckbox) {
-  [checkbox1, checkbox2, checkbox3, checkbox4].forEach(checkbox => {
+  [checkbox1, checkbox2, checkbox3, checkbox4].forEach((checkbox) => {
     if (checkbox !== selectedCheckbox) {
       checkbox.checked = false;
     }
   });
 }
-[checkbox1, checkbox2, checkbox3, checkbox4].forEach(checkbox => {
+[checkbox1, checkbox2, checkbox3, checkbox4].forEach((checkbox) => {
   checkbox.addEventListener("change", () => uncheckOthers(checkbox));
 });
 submitButton.addEventListener("click", handleSubmit);
